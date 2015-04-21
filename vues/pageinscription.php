8158690e414a4ca51d5e8e1b22b15ced8cf0f579
Sayonne <morgane.minodier@y-nov.com>
2015-04-21 17:34:04 +0200

    modification formulaire

--- vues/pageinscription.php
+++ vues/pageinscription.php
@@ -4,23 +4,37 @@ include "vues/entetepage.html";
 include "vues/logo.html";
 ?>
 <div data-role="content" id="divconnexion">       
+<form method="post" action="enregistreruser.php">
     <div data-role="fieldcontain">
-        <label for="login">Prénom</label>
+        <label for="prenom">Prénom</label>
         <input type="text" name="prenom" id="prenom" value="" />
-        <label for="login">Nom</label>
-        <input type="text" name="nom" id="nom" value="" />
-        <label for="login">Mail</label>
-        <input type="text" name="mail" id="mail" value="" />
-        <label for="login">téléphone</label>
-        <input type="text" name="tel" id="tel" value="" />
-        <label for="login">indiquer votre service</label>
-        <div data-role="controlgroup" data-type="horizontal">
-            <a  data-role="button">Recherche</a>
-            <a  data-role="button">Production</a>
-            <a  data-role="button">Commercial</a>
-            <a  data-role="button">Securite</a>
-        </div>
     </div>
+    <div data-role="fieldcontain">
+        <label for="nom">Nom</label>
+        <input type="text" name="nom" id="nom" value="" />
+    </div>
+    <div data-role="fieldcontain">
+        <label for="mail">Mail</label>
+        <input type="text" name="mail" id="mail" value="" />
+    </div>
+    <div data-role="fieldcontain">
+        <label for="tel">Téléphone</label>
+        <input type="text" name="tel" id="tel" value="" />
+    </div>
+    <div data-role="fieldcontain">
+                <label for="service">Indiquer votre service</label>
+        <fieldset data-role="controlgroup" data-type="horizontal">
+                <input type="radio" name="recherche" id="recherche" value="on" checked="checked">
+                <label for="recherche">Recherche</label>
+                <input type="radio" name="production" id="production" value="off">
+                <label for="production">Production</label>
+                <input type="radio" name="commercial" id="commercial" value="other">
+                <label for="commercial">Commercial</label>
+                <input type="radio" name="securite" id="securite" value="other">
+                <label for="securite">Sécurité</label>
+        </fieldset>
+    </div>
+</form>
         <p>
             <a href="#"  data-role="button" id="btninscription" >Envoyer</a>
          </p>
