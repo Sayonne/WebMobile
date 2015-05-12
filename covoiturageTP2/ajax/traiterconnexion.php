bf90f3b00be6b717a3ad4fc79a79020e5383687b
Aixpert <alicia.gattuso@y-nov.com>
2015-05-12 14:55:30 +0200

    tp2 covoiturage fini.

---
+++ covoiturageTP2/ajax/traiterconnexion.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+require_once '../util/fonctions.php';
+$mdp = $_REQUEST['mdp'];
+$login = $_REQUEST['login'];
+
+$user = verifuser($login, $mdp);
+if($user!="")
+ $_SESSION['id'] = $user;  
+echo $user;
+?>
